a23f056c5c3210627d48c1aef8d8fb8c1f2b0492
m3mph17 <ez4genius@gmail.com>
2023-05-26 10:24:36 +0300

    class User Key implemented

--- AbiturientTGBot/Models/User.cs
+++ AbiturientTGBot/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,10 @@ namespace AbiturientTGBot.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public long UserId { get; set; }
-        public int State { get; set; }
+        public string State { get; set; }
         public string Username { get; set; }
 
     }
