418126ef9910f98c156d58feab39e6649154384c
m3mph17 <ez4genius@gmail.com>
2023-05-27 15:56:33 +0300

    Class which contatins answers for bot. Getting strings from file

---
+++ AbiturientTGBot/Bot_QA/Answers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbiturientTGBot.Bot_QA
+{
+    public class Answers
+    {
+        public string StartMessage { get; set; }
+        public string SpecSuggest { get; set; }
+        public string NoSpecFound { get; set; }
+        public string NewApplicationSuggest { get; set; }
+        public string ProfTestSuggest { get; set; }
+        public string EndMessage { get; set; }
+    }
+}
