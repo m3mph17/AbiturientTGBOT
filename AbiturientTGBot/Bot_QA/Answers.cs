dbafe35f24dad0e4ecac12b5f950a43963136d0e
m3mph17 <ez4genius@gmail.com>
2023-05-29 13:42:17 +0300

    New bot answers

--- AbiturientTGBot/Bot_QA/Answers.cs
+++ AbiturientTGBot/Bot_QA/Answers.cs
@@ -13,6 +13,8 @@ namespace AbiturientTGBot.Bot_QA
         public string NoSpecFound { get; set; }
         public string NewApplicationSuggest { get; set; }
         public string ProfTestSuggest { get; set; }
+
+        public string ApplicationFulled { get; set; }
         public string EndMessage { get; set; }
     }
 }
