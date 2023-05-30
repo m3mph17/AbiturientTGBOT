a4640267de0824b06d5f87ffa5c98d4ec000a7f6
m3mph17 <ez4genius@gmail.com>
2023-05-30 09:58:15 +0300

    New question handlers and check for bad inputs

--- AbiturientTGBot/Handlers/ApplicationHandler.cs
+++ AbiturientTGBot/Handlers/ApplicationHandler.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AbiturientTGBot.Handlers
@@ -20,6 +21,7 @@ namespace AbiturientTGBot.Handlers
         DBService db;
         KeyboardService keyboard;
         ApplicationQuestions questions;
+        Answers answers;
 
         public ApplicationHandler(object sender, long userId, string message)
         {
@@ -30,26 +32,51 @@ namespace AbiturientTGBot.Handlers
 
             this.message = message;
             questions = bot.settings.ApplicQuestions;
+            answers = bot.settings.Answers;
         }
 
         public MessageHandle Handle()
         {
             Abiturient abiturient = db.GetAbiturient(user.Id);
-            MessageHandle messageHandle = new MessageHandle { User = user};
-
-            if (abiturient.IsFull == true)
-            {
-                messageHandle.Message = bot.settings.Answers.ApplicationFulled;
-                messageHandle.ReplyKeyboard = null;
-
-                return messageHandle;
-            }
+            MessageHandle messageHandle = new MessageHandle { User = user };
 
             if (abiturient == null)
             {
                 abiturient = new Abiturient { UserId = user.Id };
                 db.CreateNewAbiturient(abiturient);
 
+                messageHandle.Message = questions.Specizalization;
+                messageHandle.ReplyKeyboard = keyboard.SpecialityKeyboard;
+
+                return messageHandle;
+            }
+
+            if (abiturient.IsFull == true)
+            {
+                messageHandle.Message = answers.ApplicationFulled;
+                messageHandle.ReplyKeyboard = null;
+
+                return messageHandle;
+            }
+
+
+            if (abiturient.SpecId == null)
+            {
+                Specialization spec;
+                try
+                {
+                    spec = db.GetSpecizalization(message);
+                }
+                catch (Exception ex)
+                {
+                    messageHandle.Message = answers.BadSpecInput;
+                    messageHandle.ReplyKeyboard = null;
+
+                    return messageHandle;
+                }
+
+                abiturient.SpecId = spec.SpecId;
+                db.UpdateAbiturient(abiturient);
                 messageHandle.Message = questions.Fio;
                 messageHandle.ReplyKeyboard = null;
 
@@ -58,16 +85,24 @@ namespace AbiturientTGBot.Handlers
 
             if (abiturient.Surname == null)
             {
-                string[] fio = message.Split(' ');
-                abiturient.Surname = fio[0];
-                abiturient.Firstname = fio[1];
-                abiturient.Patronymic = fio[2];
+                try
+                {
+                    string[] fio = message.Split(' ');
+                    abiturient.Surname = fio[0];
+                    abiturient.Firstname = fio[1];
+                    abiturient.Patronymic = fio[2];
 
-                db.UpdateAbiturient(abiturient);
+                    db.UpdateAbiturient(abiturient);
 
-                messageHandle.Message = questions.IsMale;
-                messageHandle.ReplyKeyboard = keyboard.IsMaleKeyboard;
-
+                    messageHandle.Message = questions.IsMale;
+                    messageHandle.ReplyKeyboard = keyboard.IsMaleKeyboard;
+                }
+                catch (Exception ex)
+                {
+                    messageHandle.Message = answers.BadFioInput; // bad input answer;
+                    messageHandle.ReplyKeyboard = null;
+                }
+                
                 return messageHandle;
             }
 
@@ -78,6 +113,14 @@ namespace AbiturientTGBot.Handlers
 
                 if (message.ToLower() == "женский")
                     abiturient.IsMale = false;
+
+                if (message.ToLower() != "мужской" && message.ToLower() != "женский")
+                {
+                    messageHandle.Message = answers.BadMaleInput;
+                    messageHandle.ReplyKeyboard = keyboard.IsMaleKeyboard;
+
+                    return messageHandle;
+                }
 
                 db.UpdateAbiturient(abiturient);
 
@@ -92,30 +135,102 @@ namespace AbiturientTGBot.Handlers
                 abiturient.Address = message;
                 db.UpdateAbiturient(abiturient);
 
-                messageHandle.Message = questions.HomeNumber;
+                messageHandle.Message = questions.SchoolName;
                 messageHandle.ReplyKeyboard = null;
 
                 return messageHandle;
             }
 
-            if (abiturient.HomePhone == null)
+            if (abiturient.SchoolName == null)
             {
-                abiturient.HomePhone = Convert.ToInt64(message);
+                abiturient.SchoolName = message;
                 db.UpdateAbiturient(abiturient);
 
-                messageHandle.Message = questions.PhoneNumber;
+                messageHandle.Message = questions.SchoolAddress;
                 messageHandle.ReplyKeyboard = null;
 
+                return messageHandle;
+            }
+
+            if (abiturient.SchoolAddress == null)
+            {
+                abiturient.SchoolAddress = message;
+                db.UpdateAbiturient(abiturient);
+
+                messageHandle.Message = questions.SchoolMark;
+                messageHandle.ReplyKeyboard = null;
+
+                return messageHandle;
+            }
+
+            if (abiturient.SchoolMark == null)
+            {
+                try
+                {
+                    double mark = Convert.ToDouble(message);
+                    abiturient.SchoolMark = mark;
+
+                    db.UpdateAbiturient(abiturient);
+
+                    messageHandle.Message = questions.HomeNumber;
+                    messageHandle.ReplyKeyboard = null;
+                }
+                catch (Exception ex)
+                {
+                    messageHandle.Message = answers.BadSchoolMarkInput;
+                    messageHandle.ReplyKeyboard = null;
+
+                    return messageHandle;
+                }
+
+                return messageHandle;
+            }
+
+
+            if (abiturient.HomePhone == null)
+            {
+                try
+                {
+                    abiturient.HomePhone = Convert.ToInt64(message);
+                    db.UpdateAbiturient(abiturient);
+
+                    messageHandle.Message = questions.PhoneNumber;
+                    messageHandle.ReplyKeyboard = null;
+                }
+                catch (Exception ex)
+                {
+                    messageHandle.Message = answers.BadHomeNumberInput; // bad input;
+                    messageHandle.ReplyKeyboard = null;
+                }
+                
                 return messageHandle;
             }
 
             if (abiturient.MobilePhone == null)
             {
-                abiturient.MobilePhone = Convert.ToInt64(message);
-                db.UpdateAbiturient(abiturient);
+                try
+                {
+                    // regex pattern
+                    string pattern = @"^(375|80)(29|25|44|33)\d{7}$";
 
-                messageHandle.Message = questions.Birthday;
-                messageHandle.ReplyKeyboard = null;
+                    bool isMatch = Regex.IsMatch(message, pattern);
+
+                    if (isMatch == false)
+                    {
+                        throw new Exception("Bad phonenumber input");
+                    }
+
+                    abiturient.MobilePhone = Convert.ToInt64(message);
+                    db.UpdateAbiturient(abiturient);
+
+                    messageHandle.Message = questions.Birthday;
+                    messageHandle.ReplyKeyboard = null;
+                }
+                catch (Exception ex)
+                {
+                    messageHandle.Message = answers.BadPhoneNumberInput; // bad input;
+                    messageHandle.ReplyKeyboard = null;
+                }
 
                 return messageHandle;
             }
@@ -123,6 +238,17 @@ namespace AbiturientTGBot.Handlers
 
             if (abiturient.BirthDate == null)
             {
+                string pattern = @"^([0-2]\d|3[01])\.(0[1-9]|1[0-2])\.(19|20)\d{2}$";
+                bool isMatch = Regex.IsMatch(message, pattern);
+
+                if (isMatch == false)
+                {
+                    messageHandle.Message = answers.BadBirthdateInput; // bad input
+                    messageHandle.ReplyKeyboard = null;
+
+                    return messageHandle;
+                }
+
                 abiturient.BirthDate = message;
                 db.UpdateAbiturient(abiturient);
 
@@ -136,8 +262,16 @@ namespace AbiturientTGBot.Handlers
             {
                 if (message.ToLower() == "да")
                     abiturient.IsOrphan = true;
-                else
+                if (message.ToLower() == "нет")
                     abiturient.IsOrphan = false;
+
+                if (message.ToLower() != "да" && message.ToLower() != "нет")
+                {
+                    messageHandle.Message = answers.BadYesNoInput;
+                    messageHandle.ReplyKeyboard = keyboard.YesNoKeyboard;
+
+                    return messageHandle;
+                }
 
                 db.UpdateAbiturient(abiturient);
 
@@ -151,8 +285,16 @@ namespace AbiturientTGBot.Handlers
             {
                 if (message.ToLower() == "да")
                     abiturient.IsFromChernobyl = true;
-                else
+                if (message.ToLower() == "нет")
                     abiturient.IsFromChernobyl = false;
+
+                if (message.ToLower() != "да" && message.ToLower() != "нет")
+                {
+                    messageHandle.Message = answers.BadYesNoInput;
+                    messageHandle.ReplyKeyboard = keyboard.YesNoKeyboard;
+
+                    return messageHandle;
+                }
 
                 db.UpdateAbiturient(abiturient);
 
@@ -166,8 +308,16 @@ namespace AbiturientTGBot.Handlers
             {
                 if (message.ToLower() == "да")
                     abiturient.IsManyChildren = true;
-                else
+                if (message.ToLower() == "нет")
                     abiturient.IsManyChildren = false;
+
+                if (message.ToLower() != "да" && message.ToLower() != "нет")
+                {
+                    messageHandle.Message = answers.BadYesNoInput;
+                    messageHandle.ReplyKeyboard = keyboard.YesNoKeyboard;
+
+                    return messageHandle;
+                }
 
                 db.UpdateAbiturient(abiturient);
 
@@ -181,8 +331,16 @@ namespace AbiturientTGBot.Handlers
             {
                 if (message.ToLower() == "да")
                     abiturient.IsOpfr = true;
-                else
+                if (message.ToLower() == "нет")
                     abiturient.IsOpfr = false;
+
+                if (message.ToLower() != "да" && message.ToLower() != "нет")
+                {
+                    messageHandle.Message = answers.BadYesNoInput;
+                    messageHandle.ReplyKeyboard = keyboard.YesNoKeyboard;
+
+                    return messageHandle;
+                }
 
                 db.UpdateAbiturient(abiturient);
 
@@ -196,9 +354,17 @@ namespace AbiturientTGBot.Handlers
             {
                 if (message.ToLower() == "да")
                     abiturient.IsNeedHostel = true;
-                else
+                if (message.ToLower() == "нет")
                     abiturient.IsNeedHostel = false;
-                    
+
+                if (message.ToLower() != "да" && message.ToLower() != "нет")
+                {
+                    messageHandle.Message = answers.BadYesNoInput;
+                    messageHandle.ReplyKeyboard = keyboard.YesNoKeyboard;
+
+                    return messageHandle;
+                }
+
                 db.UpdateAbiturient(abiturient);
 
                 messageHandle.Message = questions.InvalidGroup;
@@ -209,15 +375,25 @@ namespace AbiturientTGBot.Handlers
 
             if (abiturient.InvalidGroup == null)
             {
-                if (message.ToLower() == "нет инвалидности")
-                    abiturient.InvalidGroup = 0;
-                else
-                    abiturient.InvalidGroup = Convert.ToInt32(message);
+                try
+                {
+                    if (message.ToLower() == "нет инвалидности")
+                        abiturient.InvalidGroup = 0;
+                    else
+                        abiturient.InvalidGroup = Convert.ToInt32(message);
+                }
+                catch (Exception ex)
+                {
+                    messageHandle.Message = answers.BadInvalidGroupInput;
+                    messageHandle.ReplyKeyboard = keyboard.InvalidGroupKeyboard;
 
+                    return messageHandle;
+                }
+                
                 abiturient.IsFull = true;
                 db.UpdateAbiturient(abiturient);
 
-                messageHandle.Message = bot.settings.Answers.EndMessage;
+                messageHandle.Message = answers.EndMessage;
                 messageHandle.ReplyKeyboard = null;
 
                 db.SetUserStateByTableId(user.Id, "newUser");
@@ -225,7 +401,7 @@ namespace AbiturientTGBot.Handlers
                 return messageHandle;
             }
 
-            messageHandle.Message = bot.settings.Answers.EndMessage;
+            messageHandle.Message = answers.EndMessage;
             messageHandle.ReplyKeyboard = null;
             return messageHandle;
         }
