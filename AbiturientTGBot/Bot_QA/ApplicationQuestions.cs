4969971ed78723017d54810459f031b114c98b24
m3mph17 <ez4genius@gmail.com>
2023-05-30 09:57:37 +0300

    New questions for application

--- AbiturientTGBot/Bot_QA/ApplicationQuestions.cs
+++ AbiturientTGBot/Bot_QA/ApplicationQuestions.cs
@@ -9,9 +9,13 @@ namespace AbiturientTGBot.Bot_QA
     public class ApplicationQuestions
     {
         // Questions for new applications
+        public string Specizalization { get; set; }
         public string Fio { get; set; }
         public string IsMale { get; set; }
         public string Address { get; set; }
+        public string SchoolName { get; set; }
+        public string SchoolAddress { get; set; }
+        public string SchoolMark { get; set; }
         public string Birthday { get; set; }
         public string HomeNumber { get; set; }
         public string PhoneNumber { get; set; }
