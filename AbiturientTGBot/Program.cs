a5158bffc50567b95d98219e92c0e36709d80f31
m3mph17 <ez4genius@gmail.com>
2023-06-05 13:43:01 +0300

    Now programm listents to key F5 and when its pressed - exports applications to Excel

--- AbiturientTGBot/Program.cs
+++ AbiturientTGBot/Program.cs
@@ -2,15 +2,52 @@
 using AbiturientTGBot.Service;
 using AbiturientTGBot.Models;
 using AbiturientTGBot.AppSettings;
+using AbiturientTGBot.Handlers;
+using System.Runtime.InteropServices;
 
 namespace AbiturientTGBot
 {
     static class Program
     {
+        static void KeyListenerThread()
+        {
+            [DllImport("user32.dll")]
+            static extern short GetAsyncKeyState(int keyCode);
+
+            bool keyDown = false;
+            DBService dbService = new DBService();
+
+            while (true)
+            {
+                short keyState = GetAsyncKeyState(0x74); // 0x74 is the virtual key code for F5
+                bool keyPressed = (keyState & 0x8000) != 0;
+
+                if (keyPressed && !keyDown)
+                {
+                    Console.WriteLine("Экспорт заявок в Excel начат");
+                    ExportHandler export = new ExportHandler(dbService, "Exports");
+                    export.ExportToExcel();
+
+                    keyDown = true;
+                }
+                else if (!keyPressed && keyDown)
+                {
+                    keyDown = false;
+                }
+
+                // Sleep for a short amount of time to avoid using too much CPU
+                Thread.Sleep(10);
+            }
+        }
+
         static void Main(string[] args)
         {
             KeyboardService keyboardService = new KeyboardService();
             DBService dbService = new DBService();
+
+            Thread keyListenerThread = new Thread(KeyListenerThread);
+            keyListenerThread.Start();
+
 
             BotSettings botSettings = new BotSettings(
                 cfgPath: @"Settings\Bot.cfg",
